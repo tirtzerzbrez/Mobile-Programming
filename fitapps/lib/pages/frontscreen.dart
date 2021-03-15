6360bae19cc10960e5714096163464ef0adf6fb5
user <fernandesvincent62@gmail.com>
2021-03-15 15:00:59 +0700

    fscreen

--- fitapps/lib/pages/frontscreen.dart
+++ fitapps/lib/pages/frontscreen.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 class frontscreen extends StatefulWidget {
@@ -12,8 +10,28 @@ class _frontscreenState extends State<frontscreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text("fitApps"),
+        backgroundColor: Colors.red,
+        title: Center(
+          child: Text('fitApp'),
+        )
+      )
+    );
+  }
+
+  Widget makeItem({image, tag}) {
+    return Hero(
+      tag: tag,
+      child: GestureDetector(
+        child: Container(
+          height: 250,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            image: DecorationImage(
+              image: AssetImage('assets/image/jagung.png')
+            )
+          ),
+        ),
       ),
     );
   }
