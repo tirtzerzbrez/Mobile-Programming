fda4be56194b099f8236317652ffa5b8580b53fb
aryap12 <70554757+aryap12@users.noreply.github.com>
2021-03-15 13:22:08 +0700

    push one

---
+++ fitapps/lib/pages/Workout/ORbeginner.dart
@@ -0,0 +1,29 @@
+class UpperbodyNoequip {
+  final String gambar, nama, instruksi;
+  final int index;
+
+  UpperbodyNoequip({this.gambar, this.nama, this.instruksi, this.index});
+}
+
+final upperbeginnerchest = [
+  [
+    UpperbodyNoequip(
+      index: 1,
+      gambar: "",
+      nama: "Knee-Push up",
+      instruksi: " 3 sets - 10 repetisi",
+    ),
+    UpperbodyNoequip(
+      index: 2,
+      gambar: "",
+      nama: "Knee-Wide Push up",
+      instruksi: " 3 sets - 10 repetisi",
+    ),
+    UpperbodyNoequip(
+      index: 3,
+      gambar: "",
+      nama: "Knee-Push up",
+      instruksi: " 3 sets - 10 repetisi",
+    ),
+  ],
+];
