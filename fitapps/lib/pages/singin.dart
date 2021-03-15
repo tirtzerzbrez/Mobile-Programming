021010ef90dd34e5faef4f32905cc9d483397b53
hizkia halim <hizkiahalim2412@gmail.com>
2021-03-15 14:02:59 +0700

    asset

---
+++ fitapps/lib/pages/singin.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Singin extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(image: AssetImage('image/barbelman.jpg'))),
+      ),
+    );
+  }
+}
