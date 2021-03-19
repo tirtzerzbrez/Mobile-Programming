0adbeca7c7e792592ac5c851448960a9423d2d84
aryap12 <70554757+aryap12@users.noreply.github.com>
2021-03-19 12:00:24 +0700

    push

--- fitapps/lib/pages/workout/atas.dart
+++ fitapps/lib/pages/workout/atas.dart
@@ -1,7 +1,7 @@
 import 'package:fitapps/resource/gerakan/Upper.dart';
 import 'package:flutter/rendering.dart';
 import 'package:url_launcher/url_launcher.dart';
-
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 import 'package:flutter/material.dart';
 
 class Atas extends StatefulWidget {
